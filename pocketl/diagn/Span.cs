124e61364d1527dd768f412aeb0880b147470192
Henrique Lorenzi <hlorenzi12@gmail.com>
2017-05-23 21:26:14 -0300

    add span

---
+++ pocketl/diagn/Span.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace pocketl.diagn
+{
+    public class Span
+    {
+        public string unit;
+        public uint start, end;
+
+
+        public Span(string unit, uint start, uint end)
+        {
+            this.unit = unit;
+            this.start = start;
+            this.end = end;
+        }
+
+
+        public static Span operator +(Span a, Span b)
+        {
+            if (a.unit != b.unit)
+                throw new Exception("spans point to different units");
+
+            return new Span(
+                a.unit,
+                Math.Min(a.start, b.start),
+                Math.Max(a.end, b.end));
+        }
+    }
+}
