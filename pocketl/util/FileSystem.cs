1a345f7fa71e60333ed647899feebb81dd960014
Henrique Lorenzi <hlorenzi12@gmail.com>
2017-05-23 21:02:38 -0300

    add filesystem util

---
+++ pocketl/util/FileSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+
+namespace pocketl.util
+{
+    public interface FileSystem
+    {
+        string Read(string filename);
+    }
+
+
+    public class FileSystemMock : FileSystem
+    {
+        Dictionary<string, string> files = new Dictionary<string, string>();
+
+
+        public void Add(string filename, string contents)
+        {
+            this.files.Add(filename, contents);
+        }
+
+
+        string FileSystem.Read(string filename)
+        {
+            return this.files[filename];
+        }
+    }
+}
