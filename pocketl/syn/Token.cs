8289c5e1887edc8af7620d4ec8e06de005ff4316
Henrique Lorenzi <hlorenzi12@gmail.com>
2017-05-23 21:35:31 -0300

    add token

---
+++ pocketl/syn/Token.cs
@@ -0,0 +1,35 @@
+﻿namespace pocketl.syn
+{
+    public class Token
+    {
+        public diagn.Span span;
+        public TokenKind kind;
+        public string excerpt;
+    }
+
+
+    public enum TokenKind
+    {
+        Error,
+        Whitespace,
+        Comment,
+        Identifier,
+        Number,
+        KeywordFn,
+        KeywordStruct,
+        BraceOpen,
+        BraceClose,
+        ParenOpen,
+        ParenClose,
+        Comma,
+        Dot,
+        Colon,
+        Semicolon,
+        Arrow,
+        Equal,
+        Plus,
+        Minus,
+        Asterisk,
+        Slash
+    }
+}
