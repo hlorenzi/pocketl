d7ab8c54e3b30ae341f80491e03d377d6fa28a85
Henrique Lorenzi <hlorenzi12@gmail.com>
2017-05-23 21:34:52 -0300

    add reporter

---
+++ pocketl/diagn/Reporter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+
+namespace pocketl.diagn
+{
+    public interface Reporter
+    {
+        void Error(string descr, params Caret[] carets);
+    }
+
+
+    public class Caret
+    {
+        public Span span;
+        public bool primary;
+        public string descr;
+
+
+        public Caret(Span span, bool primary = true, string descr = null)
+        {
+            this.span = span;
+            this.primary = primary;
+            this.descr = descr;
+        }
+    }
+
+
+    public class ReporterDefault : Reporter
+    {
+        List<Message> messages = new List<Message>();
+
+
+        class Message
+        {
+            public MessageKind kind;
+            public string descr;
+            public Caret[] carets;
+        }
+
+
+        enum MessageKind
+        {
+            Error,
+            Warning,
+            Info,
+            Lint
+        }
+
+
+        void Reporter.Error(string descr, params Caret[] carets)
+        {
+            this.messages.Add(new Message
+            {
+                kind = MessageKind.Error,
+                descr = descr,
+                carets = carets
+            });
+        }
+    }
+}
